1dcad450040d33f3616b1ea7eef44c3759beaadc
Ashfaq Ahmed <ashfaq@pop-os.localdomain>
2021-10-28 23:46:42 +0100

    implemented functionality to separate out premises and conclusions based on support or conflict schemes and managed to connect premises to conclusions for the support and conflict layout tab

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from elasticsearch import Elasticsearch
-from elasticsearch.serializer import JSONSerializer
-from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl import Search
 from flask import Flask, render_template, request
+from collections import OrderedDict
+import json
 
 app = Flask(__name__, static_folder="static")
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -9,7 +10,6 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 
@@ -22,34 +22,94 @@ def results():
 
         ra_nodes = []
         ca_nodes = []
-        support_edges = []
-        conflict_edges = []
+        edges = []
+        atoms = []
 
         supporting_arguments = []
         conflicting_arguments = []
 
         for item in response.hits:
             for node in item.nodes:
+                """
+                Separate RA and CA type schemes
+                """
                 if node.type == 'scheme':
                     if node.metadata.aif_json.type == 'RA':
-                        ra_nodes.append(node)
+                        scheme = {
+                            "node": node
+                        }
+                        ra_nodes.append(scheme)
                     if node.metadata.aif_json.type == 'CA':
-                        ca_nodes.append(node)
+                        scheme = {
+                            "node": node
+                        }
+                        ca_nodes.append(scheme)
             for edge in item.edges:
-                for node in ra_nodes:
-                    if node.id in edge.source_id:
-                        support_edges.append(edge)
-                for node in ca_nodes:
-                    if node.id in edge.source_id:
-                        conflict_edges.append(edge)
+                edges.append(edge)
+
             for node in item.nodes:
                 if node.type == 'atom':
-                    for edge in support_edges:
-                        if node.id in edge.target_id:
-                            supporting_arguments.append(node)
-                    for edge in conflict_edges:
-                        if node.id in edge.target_id:
-                            conflicting_arguments.append(node)
+                    atoms.append(node)
+
+        for node in ra_nodes:
+            for edge in edges:
+                if node['node']['id'] in edge.target_id or node['node']['id'] in edge.source_id:
+                    node.setdefault('connections', []).append(edge)
+
+        for node in ca_nodes:
+            for edge in edges:
+                if node['node']['id'] in edge.target_id or node['node']['id'] in edge.source_id:
+                    node.setdefault('connections', []).append(edge)
+
+        for atom in atoms:
+            for node in ra_nodes:
+                for connection in node['connections']:
+                    if atom.id in connection['source_id'] or atom.id in connection['target_id']:
+                        node.setdefault("atoms", []).append(atom)
+            for node in ca_nodes:
+                for connection in node['connections']:
+                    if atom.id in connection['source_id'] or atom.id in connection['target_id']:
+                        node.setdefault("atoms", []).append(atom)
+
+        for node in ra_nodes:
+            for connection in node['connections']:
+                if connection['target_id'] in node['node']['id']:
+                    node.setdefault("premise_edges", []).append(connection)
+                elif connection['source_id'] in node['node']['id']:
+                    node.setdefault("conclusion_edges", []).append(connection)
+            for atom in node['atoms']:
+                for premise_edge in node['premise_edges']:
+                    if atom['id'] in premise_edge['source_id']:
+                        node.setdefault("premises", []).append(atom)
+                for conclusion_edge in node['conclusion_edges']:
+                    if atom['id'] in conclusion_edge['target_id']:
+                        node["conclusion"] = atom
+            for premise in node['premises']:
+                support = {
+                    "premise": premise,
+                    "conclusion": node['conclusion']
+                }
+                supporting_arguments.append(support)
+
+        for node in ca_nodes:
+            for connection in node['connections']:
+                if connection['target_id'] in node['node']['id']:
+                    node.setdefault("premise_edges", []).append(connection)
+                elif connection['source_id'] in node['node']['id']:
+                    node.setdefault("conclusion_edges", []).append(connection)
+            for atom in node['atoms']:
+                for premise_edge in node['premise_edges']:
+                    if atom['id'] in premise_edge['source_id']:
+                        node.setdefault("premises", []).append(atom)
+                for conclusion_edge in node['conclusion_edges']:
+                    if atom['id'] in conclusion_edge['target_id']:
+                        node["conclusion"] = atom
+            for premise in node['premises']:
+                conflict = {
+                    "premise": premise,
+                    "conclusion": node['conclusion']
+                }
+                conflicting_arguments.append(conflict)
 
         return render_template('results.html', response=response, q=query, supporting_arguments=supporting_arguments,
                                conflicting_arguments=conflicting_arguments)
